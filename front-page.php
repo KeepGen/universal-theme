b1264cb84e965343fcdf6a151d0916096c2e2db7
KeepGen <keeper.gen@gmail.com>
2021-03-06 17:44:20 +0200

    Add: Added Arcticle section under Hero section using trim title feature

--- front-page.php
+++ front-page.php
@@ -9,6 +9,7 @@
 
             $myposts = get_posts([ 
                'numberposts' => 1,
+               'category_name' => 'javascript, html, css, web-design',
             ]);
 
             // Checking if posts exist
@@ -29,7 +30,7 @@
             </a>
             <div class="post-text">
                <?php the_category(); ?>
-               <h2 class="post-title"><?php the_title(); ?></h2>
+               <h2 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 41, '...') ?></h2>
                <a href="<?php echo get_the_permalink() ?>" class="more">Читать далее</a>
             </div>
             <?php 
@@ -52,6 +53,7 @@
                   $myposts = get_posts([ 
                      'numberposts' => 5,
                      'offset' => 1,
+                     'category_name' => 'javascript, html, css, web-design',
                   ]);
 
                   // Checking if posts exist
@@ -83,3 +85,38 @@
    </div>
    <!-- /.container -->
 </main>
+
+<div class="container">
+   <ul class="article-list">
+      <?php
+         // Announcing global variable
+         global $post;
+
+         $myposts = get_posts([ 
+            'numberposts' => 4,
+            'category_name' => 'articles',
+         ]);
+
+         // Checking if posts exist
+         if( $myposts ){
+            // If exist, start loop
+            foreach( $myposts as $post ){
+               setup_postdata( $post );
+      ?>
+      <!-- Выводим записи -->
+      <li class="article-item">
+         <a class="article-permalink" href="<?php echo get_the_permalink(); ?>">
+         <h4 class="article-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...') ?></h4>
+         </a>
+         <img width="65" height="65" src="<?php echo get_the_post_thumbnail_url( null, 'thumbnail' ) ?>" alt="<?php the_title(); ?>">
+      </li>
+      <?php 
+            }
+         } else {
+            ?><p>Постов нет</p><?php
+         }
+         wp_reset_postdata(); // Сбрасываем $post
+      ?>
+   </ul>
+</div>
+<!-- /.container -->
